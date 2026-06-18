887495330147863f8a0ed747e530ee7645bd5610
Harsh Singhal <harshsinghal8899@gmail.com>
2026-06-18 21:29:41 +0530

    new ques

---
+++ contest/Duck Surplus.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    vector<long long>arr(n);
+	    for(int i=0; i<n; i++)
+	    cin>>arr[i];
+	    
+	   for(int i=0; i<n-1; i++)
+       {
+        if(arr[i]>arr[i+1])
+        {
+            int temp = arr[i+1];
+            arr[i+1] = arr[i+1]+arr[i];
+            arr[i] = temp;
+        }
+       }
+       cout<<arr[n-1]<<endl;
+	}
+
+}
