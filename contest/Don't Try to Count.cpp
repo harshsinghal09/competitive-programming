e009afdcce1b77d133b605ac030d550e17c6654e
Harsh Singhal <harshsinghal8899@gmail.com>
2026-07-20 21:41:54 +0530

    new q

---
+++ contest/Don't Try to Count.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        string x,s;
+        cin>>x>>s;
+        
+        int count = 0;
+        int ans = -1;
+        for(int i=0; i<=5; i++)
+        {
+            if(x.find(s) != string::npos)
+            {
+                ans = count;
+                break;
+            }
+            count++;
+            x += x;
+            
+        }
+        cout<<ans<<endl;
+
+    }
+
+    return 0;
+}
