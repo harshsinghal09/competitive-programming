d1c6436168d601a0bf705a590909b1ec27e8acfa
Harsh Singhal <harshsinghal8899@gmail.com>
2026-07-24 16:49:36 +0530

    new code

---
+++ contest/Two Permutations.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n,a,b;
+        cin>>n>>a>>b;
+
+        if((a==n&&b==n)||(n-a-b>=2))
+            cout<<"Yes\n";
+        else
+            cout<<"No\n";
+    }
+
+    return 0;
+}
