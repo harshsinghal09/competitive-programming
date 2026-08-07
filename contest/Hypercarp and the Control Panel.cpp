0d509f3e9117eb9f0582a690f90ea2567790915e
Harsh Singhal <harshsinghal8899@gmail.com>
2026-08-07 21:44:08 +0530

    new Q

---
+++ contest/Hypercarp and the Control Panel.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int TestsNumT;
+    cin>>TestsNumT;
+
+    while(TestsNumT--)
+    {
+        int n;
+        cin>>n;
+
+        vector<int> arr(n);
+
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+
+        if(n==1)
+        {
+            cout<<1<<"\n";
+            continue;
+        }
+
+        int cur=1;
+
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]!=arr[i+1])
+                cur++;
+        }
+
+        int ans=cur;
+
+        for(int i=0;i<n-1;i++)
+        {
+            int before=0;
+
+            if(i>0 && arr[i-1]!=arr[i])
+                before++;
+
+            if(arr[i]!=arr[i+1])
+                before++;
+
+            if(i+1<n-1 && arr[i+1]!=arr[i+2])
+                before++;
+
+            swap(arr[i],arr[i+1]);
+
+            int after=0;
+
+            if(i>0 && arr[i-1]!=arr[i])
+                after++;
+
+            if(arr[i]!=arr[i+1])
+                after++;
+
+            if(i+1<n-1 && arr[i+1]!=arr[i+2])
+                after++;
+
+            ans=max(ans,cur-before+after);
+
+            swap(arr[i],arr[i+1]);
+        }
+
+        cout<<ans<<"\n";
+    }
+
+    return 0;
+}
